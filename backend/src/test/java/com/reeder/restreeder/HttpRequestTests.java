086368be9657e77c755f9b82591fb70f06f335c9
William <wlambert@marcovasco.fr>
2020-02-23 22:08:07 +0800

    feat(restreeder): migrated tests to IntelliJ

--- backend/src/test/java/com/reeder/restreeder/HttpRequestTests.java
+++ backend/src/test/java/com/reeder/restreeder/HttpRequestTests.java
@@ -1,14 +1,10 @@
 package com.reeder.restreeder;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT	)
 public class HttpRequestTests {

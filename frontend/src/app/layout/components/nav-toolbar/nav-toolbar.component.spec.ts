5412334f315f15ba86317c4927e74120c334ad73
William <wlambert@marcovasco.fr>
2020-05-07 15:22:24 +0800

    test(view): fixed navtoolbar tests

--- frontend/src/app/layout/components/nav-toolbar/nav-toolbar.component.spec.ts
+++ frontend/src/app/layout/components/nav-toolbar/nav-toolbar.component.spec.ts
@@ -1,24 +1,68 @@
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { NavToolbarComponent } from './nav-toolbar.component';
+import { MaterialModule } from '@shared/material.module';
+import * as fromAuth from '@store/reducers';
+import { selectConnectedUserName, selectIsUserConnected } from '@store/selectors';
 
 describe('NavToolbarComponent', () => {
-  let component: NavToolbarComponent;
+  let debugElement: DebugElement;
   let fixture: ComponentFixture<NavToolbarComponent>;
+  let mockStore: MockStore<fromAuth.State>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NavToolbarComponent],
+      imports: [MaterialModule],
+      providers: [provideMockStore({ initialState: { auth: fromAuth.initialState } })],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(NavToolbarComponent);
+    debugElement = fixture.debugElement;
+    mockStore = TestBed.get(Store);
+    fixture.detectChanges();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create', () => {
+    expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when a user is connected', () => {
+    beforeEach(async(() => {
+      mockStore.overrideSelector(selectConnectedUserName, 'John');
+      mockStore.overrideSelector(selectIsUserConnected, true);
+      mockStore.refreshState();
+      fixture.detectChanges();
+    }));
+
+    it(`should show the connected user's name`, () => {
+      expect(debugElement.query(By.css('.connected-user-name')).nativeElement.innerText).toContain('John');
+    });
+
+    it(`should show the disconnect button`, () => {
+      expect(debugElement.query(By.css('.disconnect'))).toBeTruthy();
+    });
+  });
+
+  describe('when no users are connected', () => {
+    beforeEach(async(() => {
+      mockStore.overrideSelector(selectConnectedUserName, '');
+      mockStore.overrideSelector(selectIsUserConnected, false);
+      mockStore.refreshState();
+      fixture.detectChanges();
+    }));
+
+    it(`should show an empty connected user's name`, () => {
+      expect(debugElement.query(By.css('.connected-user-name')).nativeElement.innerText).toEqual('');
+    });
+
+    it(`should hide the disconnect button`, () => {
+      expect(debugElement.query(By.css('.disconnect'))).toBeFalsy();
+    });
   });
 });

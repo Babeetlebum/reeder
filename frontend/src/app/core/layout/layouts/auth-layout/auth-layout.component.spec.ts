036130902a9ee9c718173e3c6d8200d2c856ee99
William <wlambert@marcovasco.fr>
2020-05-12 15:13:41 +0800

    test(view): fixed auth layout component tests

--- frontend/src/app/core/layout/layouts/auth-layout/auth-layout.component.spec.ts
+++ frontend/src/app/core/layout/layouts/auth-layout/auth-layout.component.spec.ts
@@ -1,7 +1,10 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { AuthLayoutComponent } from './auth-layout.component';
+import * as fromAuth from '@core/auth/store/auth.reducers';
 import { FooterComponent, NavToolbarComponent } from '@core/layout/components';
 import { MaterialModule } from '@shared/material.module';
 
@@ -12,7 +15,9 @@ fdescribe('AuthLayoutComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AuthLayoutComponent, FooterComponent, NavToolbarComponent],
-      imports: [MaterialModule, RouterModule.forChild([])],
+      imports: [MaterialModule, RouterModule.forRoot([])],
+      providers: [provideMockStore({ initialState: { auth: fromAuth.initialState } })],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 

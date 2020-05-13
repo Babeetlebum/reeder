ea258d6eb2f03a5b8fc1619181825bb1e625b096
William <wlambert@marcovasco.fr>
2020-05-13 16:11:18 +0800

    test(view): fixed page layout tests

--- frontend/src/app/core/layout/layout.module.ts
+++ frontend/src/app/core/layout/layout.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from '@shared/material.module';
 import { FooterComponent, NavToolbarComponent } from '@core/layout/components';
@@ -9,7 +8,7 @@ import { AuthLayoutComponent, PageLayoutComponent } from '@core/layout/layouts';
 
 @NgModule({
   declarations: [AuthLayoutComponent, PageLayoutComponent, FooterComponent, NavToolbarComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, RouterModule.forChild([])],
+  imports: [BrowserModule, MaterialModule, RouterModule.forChild([])],
   exports: [AuthLayoutComponent, PageLayoutComponent],
   providers: [],
 })
--- frontend/src/app/core/layout/layouts/page-layout/page-layout.component.spec.ts
+++ frontend/src/app/core/layout/layouts/page-layout/page-layout.component.spec.ts
@@ -1,6 +1,12 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { PageLayoutComponent } from './page-layout.component';
+import * as fromAuth from '@core/auth/store/auth.reducers';
+import { FooterComponent, NavToolbarComponent } from '@core/layout/components';
+import { MaterialModule } from '@shared/material.module';
 
 describe('PageLayoutComponent', () => {
   let component: PageLayoutComponent;
@@ -8,7 +14,10 @@ describe('PageLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PageLayoutComponent],
+      declarations: [PageLayoutComponent, FooterComponent, NavToolbarComponent],
+      imports: [MaterialModule, RouterModule.forRoot([])],
+      providers: [provideMockStore({ initialState: { auth: fromAuth.initialState } })],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 

540a3f8c51c8008bb7d148f4cc1e700bb9ac59ce
William <williamlambert88@gmail.com>
2020-07-05 22:27:18 +0800

    feat(front): added fake user api adapter

---
+++ frontend/src/app/core/auth/api/fake-api/fake-api.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { UserRepository } from '@core/auth/api/user.repository';
+import { LoginCredentials, TEST_USER, TEST_PASSWORD, User } from '@core/auth/store/auth.entities';
+
+// to use the fake-api : change the injected class in auth.module
+@Injectable({
+  providedIn: 'root',
+})
+export class FakeApiRepository implements UserRepository {
+  login(credentials: LoginCredentials): Observable<{ user: User }> {
+    return timer(1000).pipe(
+      map(() => {
+        if (credentials.email !== TEST_USER.email || credentials.password !== TEST_PASSWORD) {
+          throw new Error('Wrong credentials');
+        }
+        return { user: TEST_USER };
+      }),
+    );
+  }
+}

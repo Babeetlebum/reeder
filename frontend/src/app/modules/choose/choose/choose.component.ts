496ef033deb04d8b2f335b9682fd250997004a37
William <williamlambert88@gmail.com>
2020-08-16 00:50:25 +0800

    feat(choose): see #18 - instantiated search bar in choose component

--- frontend/src/app/modules/choose/choose/choose.component.ts
+++ frontend/src/app/modules/choose/choose/choose.component.ts
@@ -4,8 +4,12 @@ import { Observable } from 'rxjs';
 
 import * as fromChoose from '@choose/store/choose.reducers';
 import { selectBookList, selectBookListLoading } from '@choose/store/choose.selectors';
-import { getBooks, readBook } from '@choose/store/choose.actions';
+import { getBooks, readBook, searchBooks } from '@choose/store/choose.actions';
 import { Book } from '@store/models';
+
+const SEARCH_BAR_TITLE = 'Search for books';
+// tslint:disable-next-line quotemark
+const SEARCH_BAR_PLACEHOLDER = "Try 'moby', 'dickens exp' or 'rouge noir' ...";
 
 @Component({
   selector: 'reed-choose',
@@ -15,6 +19,8 @@ import { Book } from '@store/models';
 export class ChooseComponent implements OnInit {
   bookList$: Observable<Book[]>;
   bookListLoading$: Observable<boolean>;
+  searchBarTitle = SEARCH_BAR_TITLE;
+  searchBarPlaceholder = SEARCH_BAR_PLACEHOLDER;
 
   constructor(private store: Store<fromChoose.State>) {}
 
@@ -30,5 +36,8 @@ export class ChooseComponent implements OnInit {
 
   readBook(bookId: number) {
     this.store.dispatch(readBook({ bookId }));
+  }
+
+  searched(search: string): void {
   }
 }
